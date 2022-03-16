58410b9224d241b2af35f88ac907dd60296b2eda
AlexC41 <alexandru.ciuta.21@gmail.com>
2022-03-16 23:50:05 +0200

    github link modified

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ contact.innerHTML = `
     <div><a href="https://www.instagram.com/alexandru.c4/"><img src="resources/instagram logo.png" alt="instagram" class= "insta" id = "smPic"></a></div>
     <div><img src="resources/gmail logo.png" alt="email" class= "email" id = "smPic" style="object-fit: cover" title="alexandru.ciuta.21@gmail.com"></div>
     <div><img src="resources/WhatsApp logo.png" alt="telefon" class= "phone" id = "smPic"></div>
-    <div><a href="https://github.com/AlexC41/Portfolio"><img src="resources/GitHub logo.png" alt="GitHub" class= "github" id = "smPic"></a></div>
+    <div><a href="https://github.com/AlexC41/Portfolio-v2"><img src="resources/GitHub logo.png" alt="GitHub" class= "github" id = "smPic"></a></div>
     <div><a href="https://www.linkedin.com/in/alex-ciuta-6a06b1170/"><img src="resources/LinkedIn logo.png" alt="LinkedIn" class= "linkedin" id = "smPic"></a></div>
 `
 
