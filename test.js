23238c964bac85d9144db2a886083c7444380db3
AlexC41 <alexandru.ciuta.21@gmail.com>
2022-05-11 01:16:10 +0300

    Update blog + poke

---
+++ test.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let elem = document.querySelector(".menu").children
+for (let i = 0; i <= elem.length-1; i++ ) {
+    console.log(elem[i].textContent)
+}
